3b1880e163620a85bf96b684d3f41e31672fc534
xavi0n <xavchampoux@gmail.com>
2024-11-18 11:17:33 -0500

    version a jour de l'appli PC et menage dans les fichiers de l'appli PC

---
+++ PosteDeCommande/PC/Projet5e_PosteDeComande/Projet5e_PosteDeComande/PosteDeControle.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Projet5e_PosteDeComande
+{
+    partial class Poste_De_Controle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Poste_De_Controle));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ConnexionSerie_Panel = new System.Windows.Forms.Panel();
+            this.ConnexionSerie_Label = new System.Windows.Forms.Label();
+            this.VehiculeParcours_Panel = new System.Windows.Forms.Panel();
+            this.SystemOverview_Panel = new System.Windows.Forms.Panel();
+            this.Vehicle_Picture = new System.Windows.Forms.PictureBox();
+            this.VehiculeParcours_Label = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Controle_Panel = new System.Windows.Forms.Panel();
+            this.Controle_Label = new System.Windows.Forms.Label();
+            this.Chargement_Panel = new System.Windows.Forms.Panel();
+            this.Chargement_Label = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.ConnexionSerie_Panel.SuspendLayout();
+            this.VehiculeParcours_Panel.SuspendLayout();
+            this.SystemOverview_Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Vehicle_Picture)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.Controle_Panel.SuspendLayout();
+            this.Chargement_Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.ConnexionSerie_Panel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.VehiculeParcours_Panel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.19345F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.80655F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 660);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // ConnexionSerie_Panel
+            // 
+            this.ConnexionSerie_Panel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConnexionSerie_Panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ConnexionSerie_Panel.Controls.Add(this.ConnexionSerie_Label);
+            this.ConnexionSerie_Panel.Location = new System.Drawing.Point(4, 4);
+            this.ConnexionSerie_Panel.Margin = new System.Windows.Forms.Padding(4, 4, 2, 2);
+            this.ConnexionSerie_Panel.Name = "ConnexionSerie_Panel";
+            this.ConnexionSerie_Panel.Size = new System.Drawing.Size(394, 152);
+            this.ConnexionSerie_Panel.TabIndex = 0;
+            // 
+            // ConnexionSerie_Label
+            // 
+            this.ConnexionSerie_Label.AutoSize = true;
+            this.ConnexionSerie_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnexionSerie_Label.Location = new System.Drawing.Point(3, 3);
+            this.ConnexionSerie_Label.Name = "ConnexionSerie_Label";
+            this.ConnexionSerie_Label.Size = new System.Drawing.Size(117, 18);
+            this.ConnexionSerie_Label.TabIndex = 1;
+            this.ConnexionSerie_Label.Text = "Connexion Serie";
+            // 
+            // VehiculeParcours_Panel
+            // 
+            this.VehiculeParcours_Panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.VehiculeParcours_Panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tableLayoutPanel1.SetColumnSpan(this.VehiculeParcours_Panel, 2);
+            this.VehiculeParcours_Panel.Controls.Add(this.SystemOverview_Panel);
+            this.VehiculeParcours_Panel.Controls.Add(this.VehiculeParcours_Label);
+            this.VehiculeParcours_Panel.Location = new System.Drawing.Point(4, 240);
+            this.VehiculeParcours_Panel.Margin = new System.Windows.Forms.Padding(4, 2, 4, 4);
+            this.VehiculeParcours_Panel.Name = "VehiculeParcours_Panel";
+            this.VehiculeParcours_Panel.Size = new System.Drawing.Size(792, 416);
+            this.VehiculeParcours_Panel.TabIndex = 2;
+            // 
+            // SystemOverview_Panel
+            // 
+            this.SystemOverview_Panel.BackgroundImage = global::Projet5e_PosteDeComande.Properties.Resources.Apercu_Systeme_Sans_VehiculeResized;
+            this.SystemOverview_Panel.Controls.Add(this.Vehicle_Picture);
+            this.SystemOverview_Panel.Location = new System.Drawing.Point(-1, 35);
+            this.SystemOverview_Panel.Margin = new System.Windows.Forms.Padding(0);
+            this.SystemOverview_Panel.Name = "SystemOverview_Panel";
+            this.SystemOverview_Panel.Size = new System.Drawing.Size(792, 374);
+            this.SystemOverview_Panel.TabIndex = 1;
+            // 
+            // Vehicle_Picture
+            // 
+            this.Vehicle_Picture.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Vehicle_Picture.Image = global::Projet5e_PosteDeComande.Properties.Resources.LeVehiculeEnQuestion_Resized;
+            this.Vehicle_Picture.Location = new System.Drawing.Point(208, 164);
+            this.Vehicle_Picture.Margin = new System.Windows.Forms.Padding(0);
+            this.Vehicle_Picture.Name = "Vehicle_Picture";
+            this.Vehicle_Picture.Size = new System.Drawing.Size(73, 98);
+            this.Vehicle_Picture.TabIndex = 0;
+            this.Vehicle_Picture.TabStop = false;
+            // 
+            // VehiculeParcours_Label
+            // 
+            this.VehiculeParcours_Label.AutoSize = true;
+            this.VehiculeParcours_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VehiculeParcours_Label.Location = new System.Drawing.Point(3, 0);
+            this.VehiculeParcours_Label.Name = "VehiculeParcours_Label";
+            this.VehiculeParcours_Label.Size = new System.Drawing.Size(195, 18);
+            this.VehiculeParcours_Label.TabIndex = 0;
+            this.VehiculeParcours_Label.Text = "Usine - Véhicule et Parcours";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.Controle_Panel, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Chargement_Panel, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(400, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(400, 238);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // Controle_Panel
+            // 
+            this.Controle_Panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Controle_Panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controle_Panel.Controls.Add(this.Controle_Label);
+            this.Controle_Panel.Location = new System.Drawing.Point(2, 4);
+            this.Controle_Panel.Margin = new System.Windows.Forms.Padding(2, 4, 2, 2);
+            this.Controle_Panel.Name = "Controle_Panel";
+            this.Controle_Panel.Size = new System.Drawing.Size(196, 232);
+            this.Controle_Panel.TabIndex = 0;
+            // 
+            // Controle_Label
+            // 
+            this.Controle_Label.AutoSize = true;
+            this.Controle_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Controle_Label.Location = new System.Drawing.Point(3, 3);
+            this.Controle_Label.Name = "Controle_Label";
+            this.Controle_Label.Size = new System.Drawing.Size(130, 18);
+            this.Controle_Label.TabIndex = 2;
+            this.Controle_Label.Text = "Contrôle de l\'usine";
+            // 
+            // Chargement_Panel
+            // 
+            this.Chargement_Panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Chargement_Panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Chargement_Panel.Controls.Add(this.Chargement_Label);
+            this.Chargement_Panel.Location = new System.Drawing.Point(202, 4);
+            this.Chargement_Panel.Margin = new System.Windows.Forms.Padding(2, 4, 4, 2);
+            this.Chargement_Panel.Name = "Chargement_Panel";
+            this.Chargement_Panel.Size = new System.Drawing.Size(194, 232);
+            this.Chargement_Panel.TabIndex = 1;
+            // 
+            // Chargement_Label
+            // 
+            this.Chargement_Label.AutoSize = true;
+            this.Chargement_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chargement_Label.Location = new System.Drawing.Point(3, 3);
+            this.Chargement_Label.Name = "Chargement_Label";
+            this.Chargement_Label.Size = new System.Drawing.Size(166, 18);
+            this.Chargement_Label.TabIndex = 3;
+            this.Chargement_Label.Text = "Chargement du véhicule";
+            // 
+            // Poste_De_Controle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 660);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Poste_De_Controle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Poste de controle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ConnexionSerie_Panel.ResumeLayout(false);
+            this.ConnexionSerie_Panel.PerformLayout();
+            this.VehiculeParcours_Panel.ResumeLayout(false);
+            this.VehiculeParcours_Panel.PerformLayout();
+            this.SystemOverview_Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Vehicle_Picture)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.Controle_Panel.ResumeLayout(false);
+            this.Controle_Panel.PerformLayout();
+            this.Chargement_Panel.ResumeLayout(false);
+            this.Chargement_Panel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel ConnexionSerie_Panel;
+        private System.Windows.Forms.Panel VehiculeParcours_Panel;
+        private System.Windows.Forms.Label VehiculeParcours_Label;
+        private System.Windows.Forms.Label ConnexionSerie_Label;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel Controle_Panel;
+        private System.Windows.Forms.Panel Chargement_Panel;
+        private System.Windows.Forms.Label Controle_Label;
+        private System.Windows.Forms.Label Chargement_Label;
+        private System.Windows.Forms.Panel SystemOverview_Panel;
+        private System.Windows.Forms.PictureBox Vehicle_Picture;
+    }
+}
+
---
+++ PosteDeCommande/PC/Projet5e_PosteDeComande/Projet5e_PosteDeComande/PosteDeControle.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet5e_PosteDeComande
+{
+    public partial class Poste_De_Controle : Form
+    {
+        char[] cTrameOut;
+        char[] cTrameIn;
+        public enum StationState
+        {
+            WEIGHING_STATION = 0,
+            OTW_TO_WEIGHING = 1,
+            OTW_TO_SORTING = 2,
+            SORTING_STATION = 3
+        }
+        StationState currentState = StationState.WEIGHING_STATION;
+        float fPoidsRondelle;
+        bool bTrameAEnvoyer;
+
+        public Poste_De_Controle()
+        {
+            InitializeComponent();
+            PopulateCOMPorts();
+        }
+
+        private void PopulateCOMPorts()
+        {
+            COMPorts_comboBox.Items.AddRange(SerialPort.GetPortNames());
+            if (COMPorts_comboBox.Items.Count > 0)
+            {
+                COMPorts_comboBox.SelectedIndex = 0; // Select the first port by default
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //AJOUTER APPEL A CETTE FONCTION QUAND ON RECOIT DES INFOS SUR LA POSITION DU VEHICULE DE LA PART DU BBB
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////
+        private void AjustementPositionVehicule()
+        {
+            switch (currentState)
+            {
+                case StationState.WEIGHING_STATION:
+                    // Set position for WEIGHING_STATION
+                    Vehicle_Picture.Location = new Point(208, 164);
+                    Vehicle_Picture.RightToLeft = RightToLeft.No;
+                    break;
+
+                case StationState.OTW_TO_WEIGHING:
+                    // Set position for OTW_TO_WEIGHING
+                    Vehicle_Picture.Location = new Point(356, 174);
+                    Vehicle_Picture.RightToLeft = RightToLeft.No;
+                    break;
+
+                case StationState.OTW_TO_SORTING:
+                    // Set position for OTW_TO_SORTING
+                    Vehicle_Picture.Location = new Point(356, 174);
+                    Vehicle_Picture.RightToLeft = RightToLeft.Yes;
+                    break;
+
+                case StationState.SORTING_STATION:
+                    // Set position for SORTING_STATION
+                    Vehicle_Picture.Location = new Point(521, 163);
+                    Vehicle_Picture.RightToLeft = RightToLeft.Yes;
+                    break;
+            }
+            Vehicle_Picture.Refresh();  //Refreshes the picture's orientation and position after the switch/case
+        }
+        private void AjustementDuPoids()
+        {
+            //Poids_TextBox.Text = fPoidsRondelle.ToString();
+        }
+
+        private void EnvoiDeLaTrame()
+        {
+            // Convert the character array (assumed to exist) to a byte array
+            byte[] data = Array.ConvertAll(cTrameOut, c => (byte)c);
+
+            // Get the selected COM port from a dropdown menu (example: comboBoxCOMPorts)
+            string selectedPort = COMPorts_comboBox.SelectedItem?.ToString();
+
+            try
+            {
+                // Open and configure the serial port
+                using (SerialPort serialPort = new SerialPort(selectedPort, 19200, Parity.None, 8, StopBits.One))
+                {
+                    serialPort.Open();
+
+                    // Send the byte array
+                    serialPort.Write(data, 0, data.Length);
+                    MessageBox.Show("Message sent successfully at 19200 baud.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to send message: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ReceptionDeLaTrame()
+        {
+            
+        }
+    }
+}
