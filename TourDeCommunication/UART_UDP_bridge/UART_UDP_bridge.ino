97cdaba9c9b503656430b88c909ee5bf7ad3993d
xavi0n <xavchampoux@gmail.com>
2024-11-26 16:18:43 -0500

    écriture de fonctions d'initialisation pour le programme, de reception UDP et de transmission UART

--- TourDeCommunication/UART_UDP_bridge/UART_UDP_bridge.ino
+++ TourDeCommunication/UART_UDP_bridge/UART_UDP_bridge.ino
@@ -5,29 +5,52 @@
 #define LED 13
 #define BOUTON 14
 
+#define UDP_PACKET_SIZE 8
+#define UART_PACKET_SIZE 10
+
 //NETWORK CREDENTIALS
-const char* ssid = "Routeur627";
-const char* ssid = "Routeur627";
+const char* SsidAP = "TourDeComEquipe3";
+const char* PasswordAP = "TourDeComEquipe3";
+const char* LocalIpAP = "192.168.27.1 ";
+const char* GatewayAP = "192.168.27.1"; // Même que l'addresse IP, c'est voulu
+const char* SubnetAP = "255.255.255.0";
 
-//UDP settings
+//UDP related variables
 WiFiUDP udp;
-unsigned int ListenPort = 10024;     // ESP32 listens here
-unsigned int pcPort = 10025;         // Send to PC's listening port
-const char* pcIP = "192.168.1.112";  // Replace with PC's IP
-char incomingPacket[255];            // Buffer for incoming packets
+unsigned int ListenPort = 10024;          // ESP32 listens here
+unsigned int VehiclePort = 10024;         // Send to Vehicle's listening port
+const char* VehicleIP = "192.168.1.127";  //
+char incomingPacket[255];                 // Buffer for incoming packets
+char outgoingPacket[255];                 // Buffer for outgoing packets
 
+//UART related variables
+HardwareSerial STMSerial(1);  // Using UART1 to avoid conflict with debugging port, which is UART0
+char UARTFrameIN[10];         // Buffer for incoming UART frames
+char UARTFrameOUT[10];        // Buffer for outgoing UART frames
+unsigned char ucCheckSum;     // Temporary variable for calculating CheckSums
+
+//Misc variables
+int i;  // Temp variable, mainly for for loops
 
 void setup() {
+  //Start serial comunication port with PC (via UART0, for debugging)
+  Serial.begin(115200);
+
+  //Start serial communication port with STM32 (via UART1, for data communication)
+  STMSerial.begin(115200, SERIAL_8N1, 16, 17);                  // Baud rate, data bits, Rx pin (GPIO16), Tx pin (GPIO17)
+  Serial.println("Serial Communication with STM32 started");    // Start indicator on debug serial port
+
   //INITIALISATIONS DES GPIO
   pinMode(LED, OUTPUT);
   pinMode(BOUTON, INPUT);
-  bLedState = LOW;
-  digitalWrite(LED, bLedState);
 
-  //CONNEXION AU WIFI ET RECUPERATION DE L'ADDRESSE IP
-  Serial.begin(115200);
-  Serial.println("Connexion au routeur");
-  WiFi.begin(ssid, password);
+  //CONNEXION AU WIFI EN MODE ACCESS POINT
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(SsidAP, PasswordAP);
+  delay(100);
+  WiFi.softAPConfig(LocalIpAP, GatewayAP, SubnetAP);
+  Serial.print("Soft-AP IP address = ");
+  Serial.println(WiFi.softAPIP());
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -43,38 +66,67 @@ void setup() {
   // Start UDP and listen on esp32ListenPort
   udp.begin(ListenPort);
   Serial.printf("Now listening on UDP port %d\n", ListenPort);
+
+  //
 }
 
 void loop() {
-  // Check if a UDP packet has been received
-  int packetSize = udp.parsePacket();
+}
 
-  if (packetSize) {  // If the packet size is greater than 0, data has been received
-    // Read the incoming packet
-    int len = udp.read(incomingPacket, 255);
-    if (len > 0) {
-      for (int i = 0; i < len; i++) {       //Print received data to serial port
-        Serial.print("Byte ");
-        Serial.print(i);
-        Serial.print(": ");
-        Serial.println(incomingPacket[i], HEX); // Print in hexadecimal format
+
+void ReadUDP() {
+  static unsigned long lastTime = 0;  // Stores the last time the function executed
+  unsigned long currentTime = millis();
+
+  // Execute only if 100 millisecond have been elapsed
+  if (currentTime - lastTime >= 100) {
+    lastTime = currentTime;  // Update the last execution time
+
+    // Check if a UDP packet has been received
+    int packetSize = udp.parsePacket();
+
+    if (packetSize) {  // If the packet size is greater than 0, data has been received
+      int len = udp.read(incomingPacket, 255);
+      if (len > 0) {
+        // Print received data for debugging
+        for (int i = 0; i < len; i++) {
+          Serial.print("Byte ");
+          Serial.print(i);
+          Serial.print(": ");
+          Serial.println(incomingPacket[i], HEX);  // Print in hexadecimal format
+        }
+
+        // Process the packet if it matches the expected size
+        if (len == UDP_PACKET_SIZE) {
+          for (int i = 0; i < UDP_PACKET_SIZE; i++) {
+            UARTFrameOUT[i + 2] = incomingPacket[i];  // Write UDP data into the outgoing UART frame
+          }
+          WriteUART();  // Send data over UART
+        } else {
+          Serial.println("Warning: Packet size does not match expected size.");
+        }
       }
     }
+  }
 }
 
-
-void ReadUDP(){
-
+void WriteUDP() {
 }
 
-void WriteUDP(){
+void ReadUART() {
+}
+
+void WriteUART() {
+  UARTFrameOUT[0] = 0x24;     // Start condition for UART frames
+  UARTFrameOUT[1] = 0x03;     // Identifier for Control Tower/Transports gestion unit on CAN network
+  ucCheckSum = 0;             // Reset stored checksum value to ensure it is calculated correctly each time
+
+  for (i = 2; i < (UART_PACKET_SIZE-1); i++){     // Calculate CheckSum for outgoing UART data frame
+    ucCheckSum = ucCheckSum + UARTFrameOUT[i];    // CheckSum does not include either the start condition of the identifier
+  }
+  UARTFrameOUT[UART_PACKET_SIZE-1] = ucCheckSum;  //Checksum as the last byte of the outgoing UART data frame
   
-}
-
-void ReadUART(){
-  
-}
-
-void WriteUART(){
-  
+  for (i = 0; i < UART_PACKET_SIZE; i++){
+    STMSerial.write(UARTFrameOUT[i]);             // Send each byte individually
+  }
 }
