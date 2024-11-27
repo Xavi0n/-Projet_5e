28ab2612b54ad942b98deafd25631b8f7169529c
xavi0n <xavchampoux@gmail.com>
2024-11-27 10:53:41 -0500

    écriture des fonctions loop(), ReadUART et WriteUDP. Corrections et modifications mineures dans ReadUDP version finale du code pour l'esp32 de la tour de controle si tout fonctionne comme prévu

--- TourDeCommunication/UART_UDP_bridge/UART_UDP_bridge.ino
+++ TourDeCommunication/UART_UDP_bridge/UART_UDP_bridge.ino
@@ -7,12 +7,14 @@
 
 #define UDP_PACKET_SIZE 8
 #define UART_PACKET_SIZE 10
+#define UART_START_CONDITION 0x24
+#define CONTROL_TOWER_ID 0x03
 
 //NETWORK CREDENTIALS
 const char* SsidAP = "TourDeComEquipe3";
 const char* PasswordAP = "TourDeComEquipe3";
 const char* LocalIpAP = "192.168.27.1 ";
-const char* GatewayAP = "192.168.27.1"; // Même que l'addresse IP, c'est voulu
+const char* GatewayAP = "192.168.27.1";  // Même que l'addresse IP, c'est voulu
 const char* SubnetAP = "255.255.255.0";
 
 //UDP related variables
@@ -21,7 +23,7 @@ unsigned int ListenPort = 10024;          // ESP32 listens here
 unsigned int VehiclePort = 10024;         // Send to Vehicle's listening port
 const char* VehicleIP = "192.168.1.127";  //
 char incomingPacket[255];                 // Buffer for incoming packets
-char outgoingPacket[255];                 // Buffer for outgoing packets
+char outgoingPacket[8];                   // Buffer for outgoing packets
 
 //UART related variables
 HardwareSerial STMSerial(1);  // Using UART1 to avoid conflict with debugging port, which is UART0
@@ -37,8 +39,8 @@ void setup() {
   Serial.begin(115200);
 
   //Start serial communication port with STM32 (via UART1, for data communication)
-  STMSerial.begin(115200, SERIAL_8N1, 16, 17);                  // Baud rate, data bits, Rx pin (GPIO16), Tx pin (GPIO17)
-  Serial.println("Serial Communication with STM32 started");    // Start indicator on debug serial port
+  STMSerial.begin(115200, SERIAL_8N1, 16, 17);                // Baud rate, data bits, Rx pin (GPIO16), Tx pin (GPIO17)
+  Serial.println("Serial Communication with STM32 started");  // Start indicator on debug serial port
 
   //INITIALISATIONS DES GPIO
   pinMode(LED, OUTPUT);
@@ -66,21 +68,20 @@ void setup() {
   // Start UDP and listen on esp32ListenPort
   udp.begin(ListenPort);
   Serial.printf("Now listening on UDP port %d\n", ListenPort);
-
-  //
 }
 
 void loop() {
+  ReadUDP();
+  ReadUART();
 }
 
-
 void ReadUDP() {
-  static unsigned long lastTime = 0;  // Stores the last time the function executed
-  unsigned long currentTime = millis();
+  static unsigned long UDPlastTime = 0;  // Stores the last time the function executed
+  unsigned long UDPcurrentTime = millis();
 
   // Execute only if 100 millisecond have been elapsed
-  if (currentTime - lastTime >= 100) {
-    lastTime = currentTime;  // Update the last execution time
+  if (UDPcurrentTime - UDPlastTime >= 100) {
+    UDPlastTime = UDPcurrentTime;  // Update the last execution time
 
     // Check if a UDP packet has been received
     int packetSize = udp.parsePacket();
@@ -103,7 +104,7 @@ void ReadUDP() {
           }
           WriteUART();  // Send data over UART
         } else {
-          Serial.println("Warning: Packet size does not match expected size.");
+          Serial.println("Warning: Received UDP packet size does not match expected size.");
         }
       }
     }
@@ -111,22 +112,64 @@ void ReadUDP() {
 }
 
 void WriteUDP() {
+  // Send outgoing packet to the vehicle (or other destination)
+  udp.beginPacket(VehicleIP, VehiclePort);                            // Vehicle's IP and port
+  udp.write((const uint8_t*)outgoingPacket, sizeof(outgoingPacket));  // Send the packet
+  udp.endPacket();                                                    // Finalize the packet
+
+  Serial.println("Outgoing packet sent to the vehicle.");
 }
 
 void ReadUART() {
+  static unsigned long UARTlastTime = 0;  // Stores the last time the function executed
+  unsigned long UARTcurrentTime = millis();
+
+  // Execute only if 100 millisecond have been elapsed
+  if (UARTcurrentTime - UARTlastTime >= 100) {
+    UARTlastTime = UARTcurrentTime;  // Update the last execution time
+
+    if (STMSerial.available() == UART_PACKET_SIZE) {  // Check if expected amount of bytes has been received
+      for (i = 0; i < UART_PACKET_SIZE; i++) {
+        UARTFrameIN[i] = STMSerial.read();  // Read each received byte to UART incoming frame buffer
+        Serial.print("Byte ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(UARTFrameIN[i], HEX);  // Print each received byte in hexadecimal format
+      }
+      if (UARTFrameIN[0] == UART_START_CONDITION) {  // Process data only if first byte is expected UART frame start condition
+        ucCheckSum = 0;                              // Reset CheckSum value to ensure it is calculated correctly each time
+        for (i = 2; i < (UART_PACKET_SIZE - 1); i++) {
+          ucCheckSum += UARTFrameIN[i];  // Accumulates the received CheckSum
+        }
+        if (ucCheckSum == UARTFrameIN[UART_PACKET_SIZE - 1]) {  // Check if calculated checksum matches with received checksum
+          for (i = 0; i < UDP_PACKET_SIZE; i++) {               // Copy received UART frame minus start condition, amount of bytes and checksum
+            outgoingPacket[i] = UARTFrameIN[i + 2];
+          }
+          Serial.println("Received UART data, copied to outgoing UDP packet successfully");
+          WriteUDP();
+        } else {
+          Serial.println("Warning: Received UART CheckSum does not match calculated CheckSum");
+        }
+      } else {
+        Serial.println("Warning: Received UART start condition does not match expected start condition");
+      }
+    } else {
+      Serial.println("Warning: Received UART frame size does not match expected size");
+    }
+  }
 }
 
 void WriteUART() {
-  UARTFrameOUT[0] = 0x24;     // Start condition for UART frames
-  UARTFrameOUT[1] = 0x03;     // Identifier for Control Tower/Transports gestion unit on CAN network
-  ucCheckSum = 0;             // Reset stored checksum value to ensure it is calculated correctly each time
+  UARTFrameOUT[0] = UART_START_CONDITION;  // Start condition for UART frames
+  UARTFrameOUT[1] = CONTROL_TOWER_ID;      // Identifier for Control Tower/Transports gestion unit on CAN network
+  ucCheckSum = 0;                          // Reset stored checksum value to ensure it is calculated correctly each time
 
-  for (i = 2; i < (UART_PACKET_SIZE-1); i++){     // Calculate CheckSum for outgoing UART data frame
+  for (i = 2; i < (UART_PACKET_SIZE - 1); i++) {  // Calculate CheckSum for outgoing UART data frame
     ucCheckSum = ucCheckSum + UARTFrameOUT[i];    // CheckSum does not include either the start condition of the identifier
   }
-  UARTFrameOUT[UART_PACKET_SIZE-1] = ucCheckSum;  //Checksum as the last byte of the outgoing UART data frame
-  
-  for (i = 0; i < UART_PACKET_SIZE; i++){
-    STMSerial.write(UARTFrameOUT[i]);             // Send each byte individually
+  UARTFrameOUT[UART_PACKET_SIZE - 1] = ucCheckSum;  //Checksum as the last byte of the outgoing UART data frame
+
+  for (i = 0; i < UART_PACKET_SIZE; i++) {
+    STMSerial.write(UARTFrameOUT[i]);  // Send each byte individually
   }
 }
